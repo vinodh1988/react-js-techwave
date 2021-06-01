3b803b6ba5719c61c7f2d9308a9a3518756568de
vinodh1988 <k.c.vinodh@hotmail.com>
2021-06-01 10:22:01 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import Box from './Components/Box'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+     render(){
+         return(
+           <div>
+              <h1>React First Component is running</h1>
+              <Box></Box>
+              <Box></Box>
+              <Box></Box>
+              <Box></Box>
+           </div>
+         )
+     }
 }
-
 export default App;
---
+++ src/Components/Box.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './styles/box.css'
+
+class Box extends React.Component{
+    render(){
+        return(
+        <div class="box">
+            this is box
+        </div>
+        );
+    }
+}
+
+export default Box;
